1c1599e8ae9fb6d902838318c1f845f62f13f17a
KushTrip <135483015+KushTrip@users.noreply.github.com>
2026-09-02 16:17:43 +0545

    added \n to print statements

--- ingest.py
+++ ingest.py
@@ -12,20 +12,20 @@ collection = db['sensor_telemetry']
 def run_batch_job():
     try:
         # Step A: Enforce Idempotency by dropping the collection if it exists
-        print("1. Clearing old data to prevent duplicates...")
+        print("\n1. Clearing old data to prevent duplicates...")
         collection.delete_many({})
 
         # Step B: Read the CSV file into a DataFrame
-        print("2. Reading CSV file (this might take a few seconds for 405k rows)...")
+        print("\n2. Reading CSV file (this might take a few seconds for 405k rows)...")
         df = pd.read_csv('sensor_data.csv')
 
         # Step B2: Check if the file is completely empty
         if df.empty:
-            print("FAILURE: The file 'sensor_data.csv' exists but contains zero data rows. Aborting batch job.")
+            print("\nFAILURE: The file 'sensor_data.csv' exists but contains zero data rows. Aborting batch job.")
             return  # This stops the script safely
 
         # Step C: Data Quality check to ensure the DataFrame is not empty
-        print("Scanning for missing values and half-broken rows...")
+        print("\n3. Scanning for missing values and half-broken rows...")
         initial_row_count = len(df)
 
         # Drop any row with missing value 
@@ -35,26 +35,26 @@ def run_batch_job():
         # calculate and log the broken rows
         broken_rows = initial_row_count - cleaned_row_count
         if broken_rows > 0:
-            print(f"  -> WARNING: {broken_rows} rows were dropped due to missing values.")
+            print(f"\n  -> WARNING: {broken_rows} rows were dropped due to missing values.")
         else:
-            print("  -> SUCCESS: Data is 100% clean with no missing values.")
+            print("\n  -> SUCCESS: Data is 100% clean with no missing values.")
 
         # Step D: Transform the DataFrame to match the MongoDB schema
-        print("3. Transforming data to match MongoDB document format...")
+        print("\n4. Transforming data to match MongoDB document format...")
         data_dict = df_cleaned.to_dict("records")
 
         # Step E: Load the transformed data into MongoDB
-        print("4. Inserting massive Batch of data into MongoDB...")
+        print("\n5. Inserting massive Batch of data into MongoDB...")
         collection.insert_many(data_dict)
-
+    
         # Step F: Verify the insertion by counting the documents in the collection
         total_records = collection.count_documents({})
-        print(f"SUCCESS: {total_records} clean records inserted into EnviroLens DB!")
+        print(f"\nSUCCESS: {total_records} clean records inserted into EnviroLens DB!")
 
     except FileNotFoundError:
-        print("ERROR: The file 'sensor_data.csv' was not found. Please ensure the file exists in the correct directory.")
+        print("\nERROR: The file 'sensor_data.csv' was not found. Please ensure the file exists in the correct directory.")
     except Exception as e:
-        print(f"ERROR: An unexpected error occurred: {e}")
+        print(f"\nERROR: An unexpected error occurred: {e}")
 
 
 if __name__ == "__main__":
